2f6c3f9ff55ea62215f8a285481c16df649e6629
whwo2142@naver.com <whwo2142@naver.com>
2021-10-12 10:07:34 +0900

    초기 커밋.

---
+++ gong/211012.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#define CRT_SECURE_NO_WARNING
+
+int j, i, a, b;
+
+int prm(int a, int b)
+{
+	for (int i;  i < b;  b++) {		
+		for (int j; j < a; a++) {
+			printf("*");
+		}
+		printf("\n");
+
+	}
+}
+
+int main() {
+	int a, b;
+	printf("길이는");
+	scanf_s("%d" ,& a);
+	printf("높이는");
+	scanf_s("%d", &b);
+
+	prm(a, b);
+
+	return 0;
+}
