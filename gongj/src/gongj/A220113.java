1f415c06feadbad022cffd809471859a8213a000
whwo2142@naver.com <whwo2142@naver.com>
2022-01-13 13:52:52 +0900

    220113

--- gongj/src/gongj/A220113.java
+++ gongj/src/gongj/A220113.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class A220113 { 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 
+		int N = Integer.parseInt(br.readLine());//횟수 받기
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");//공백을 기준으로 문자열분리
+		
+		int index = 0;//배열숫자
+		int[] arr = new int[N];//배열생성
+		while(st.hasMoreTokens()) {//토큰 다 받을때까지
+			arr[index] = Integer.parseInt(st.nextToken());//토큰 계속 넣어주고
+			index++;//배열위치 바꿔주고
+		}
+		
+		Arrays.sort(arr);//배열 오름차순 정렬
+		System.out.print(arr[0] + " " + arr[N - 1]);//첫번째와 마지막으로
+	}
 
 //	public static void main(String[] args) { //10818 
 //		int a=0;
