b3eee4a661d0ad84dc91dbc63ed34147f01dc05e
whwo2142@naver.com <whwo2142@naver.com>
2022-01-13 11:19:53 +0900

    220113

---
+++ gongj/src/gongj/A220113.java
@@ -0,0 +1,69 @@
+package gongj;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A220113 {
+
+//	public static void main(String[] args) { // 10818
+//		int a=0;
+//		int arr[];
+//		int max=-1000001;
+//		int low=1000001;
+//		Scanner sc=new Scanner(System.in);
+//		a=sc.nextInt();
+//		arr = new int[a];
+//		
+//		for(int i=0;i<a;i++) {
+//			arr[i]=sc.nextInt();
+//		}
+//		
+//		for (int i=0;i<a;i++){
+//			if(arr[i]>max) {
+//				max=arr[i];
+//			}
+//			if(arr[i]<low) {
+//				low=arr[i];
+//			}
+//		}
+//		Arrays.sort(arr);//배열 정리하는것. 첫번째,마지막하면됨.
+//		System.out.println(low +" "+ max);
+//	}
+	
+	
+//	public static int add(int[] scores) {
+//		int sum=0;
+//		for(int i=0;i<3;i++) {
+//			sum+=scores[i];
+//		}
+//		return sum;
+//		
+//	}
+//	
+//	public static void main(String[] args) 
+//	{
+//		int[] scores;
+//		scores = new int[]{83,90,87};
+//		int sum1=0;
+//		for(int i=0;i<3;i++) {
+//			sum1+=scores[i];
+//		}
+//		System.out.println("총합:" + sum1);
+//		
+//		int sum2 = add(new int[] {83,90,87});
+//		System.out.println("총합:" + sum2);
+//		System.out.println();
+//		}		
+//		int a=0;
+//		Scanner sc = new Scanner(System.in);
+//		a=sc.nextInt();
+//		for (int i = 1; i <= a; i++) {
+//			System.out.println(i);
+//		}
+		
+
+}
+
+
