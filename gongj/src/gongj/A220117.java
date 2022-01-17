b890747dbedf6344a9584fb168ee21b620cf9a77
whwo2142@naver.com <whwo2142@naver.com>
2022-01-17 10:45:21 +0900

    220117

---
+++ gongj/src/gongj/A220117.java
@@ -0,0 +1,47 @@
+package gongj;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class A220117 {
+
+	public static void main(String[] args) throws IOException {
+		String f = "factor\n";
+		String m = "multiple\n";
+		String n = "neither\n";
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+ 
+		while(true) {
+			st = new StringTokenizer(br.readLine()," ");
+			
+			int first = Integer.parseInt(st.nextToken());
+			int second = Integer.parseInt(st.nextToken());
+			
+			if(first == 0 && second == 0) break;
+			
+			if(second % first == 0) {
+				sb.append(f);
+			}
+			else if(first % second == 0) {
+				sb.append(m);
+			}
+			else {
+				sb.append(n);
+			}
+			
+		}
+		System.out.println(sb);
+	}
+	
+
+}
+
