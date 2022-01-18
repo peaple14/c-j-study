4d9df7e2a22604ae03ca77da26fb636729bf7342
whwo2142@naver.com <whwo2142@naver.com>
2022-01-18 10:46:44 +0900

    220118

---
+++ gongj/src/gongj/A220118.java
@@ -0,0 +1,74 @@
+package gongj;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+//대2때 못한과제
+//이해하기위한 복잡함
+class Mul{
+	public static Mul mul;
+
+	void M(int a,int b) {
+		System.out.println(a*b);
+	}
+}
+
+class Div extends Mul{
+	void D(int a,int b) {
+		System.out.println(a/b);
+	}
+}
+
+class Minus extends Div{
+	void M(int a,int b) {
+		System.out.println(a-b);
+	}
+}
+
+class Sum extends Minus{
+	void S(int a,int b) {
+		System.out.println(a+b);
+	}
+}
+
+class Cal extends Sum{
+	String sign;
+	int a=0,b=0;
+	void go(String N,int a,int b) {
+		switch (N) {
+		case "+" : Sum sum = new Sum();
+		sum.S(a, b);
+		break;
+		
+		case"-":Minus minus = new Minus();
+		minus.M(a, b);
+		break;
+		
+		case"/":Div div = new Div();
+		div.D(a, b);
+		break;
+		
+		case"*":Mul mul = new Mul();
+		mul.M(a,b);
+		break;
+			
+		}
+	}
+}
+
+public class A220118 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N = br.readLine();
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		Cal cal = new Cal();
+		cal.go(N, a, b);
+	}
+
+}
