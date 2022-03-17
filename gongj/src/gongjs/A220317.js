ce5306e0985e10a7e2dd22cead687854218bfbe1
whwo2142@naver.com <whwo2142@naver.com>
2022-03-17 10:44:54 +0900

    2022-03-17

---
+++ gongj/src/gongjs/A220317.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+ /*let raman = {
+	eats: "가능"
+};
+function hot(name,cal){
+	this.name = name;
+	this.cal = cal;
+}
+
+hot.prototype = raman; //상속한다는뜻
+
+let Hot = new hot("신라면",500);
+
+alert(Hot.name + "은" + Hot.cal + "칼로리이고 맛있게 먹기가" + Hot.eats + "하다");*/
+
+/*try {
+
+  alert('에러가 없을시'); 
+
+
+} catch(err) {
+
+  alert('에러가 있을시');
+
+} finally{
+	alert('위의것이 실행되든 안되든 이건실행');
+}*/
+
+/*throw "eeror2" // 예외처리 이것도 가능*/
+
