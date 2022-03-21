391b78411be2a49fbdc3017cc83d1f2eeae9646d
whwo2142@naver.com <whwo2142@naver.com>
2022-03-21 10:48:14 +0900

    22-03-21

---
+++ gongj/src/gongjs/A220321.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+ 
+ //기본방식
+/*function first(){
+	alert("1");
+}
+
+function second(){
+	alert("2");
+}
+first();
+second();*/
+
+//콜백함수
+/*function first(scr){
+	alert("1");
+	scr();
+}
+
+function second(){
+	alert("2");
+}
+first(second);*/
+
+//길어지거나 쓰이지 않을곳엔 프라미스
+
