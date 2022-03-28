7163879456eb6433dc983d4b0d1f9f22f3716c0b
whwo2142@naver.com <whwo2142@naver.com>
2022-03-28 12:41:28 +0900

    220328

---
+++ gongj/src/gongjs/A220328.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+ 
+ 
+ //document.body.style.background = "red";//배경색 변경
+ 
+ //alert(window.innerHeight); // 창 높이
+ 
+ //alert(location.href); // 현재주소 알려주는것
+ 
+ //location.href = "https://wikipedia.org"; // 여기로 넘어감
+ 
+//DOM에서 null은 해당노드없음을 뜻함 
+
